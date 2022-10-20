d74630ddb3cf9b1ed5900858f70c03197d62f6d5
Maksim Shibanov <asusib74@gmail.com>
2022-10-20 14:41:12 +0500

    HW 11.1 Fix DeadlockExample1

--- src/com/urise/webapp/DeadlockExample1.java
+++ src/com/urise/webapp/DeadlockExample1.java
@@ -5,27 +5,44 @@ public class DeadlockExample1 {
     private static final Object LOCK2 = new Object();
 
     public static void main(String[] args) {
-        DeadlockExample1 deadlock = new DeadlockExample1();
-        new Thread(deadlock::operation1, "T1").start();
-        new Thread(deadlock::operation2, "T2").start();
+        new DeadlockExample1().startThreads();
+
     }
 
-    private void operation1() {
+    private void startThreads() {
+        Runnable runnable1 = () -> {
+            print("start");
+            operation(LOCK1, LOCK2);
+            print("end");
+        };
+        Runnable runnable2 = () -> {
+            print("start");
+            operation(LOCK2, LOCK1);
+            print("end");
+        };
+        Thread thread1 = new Thread(runnable1, "T1");
+        Thread thread2 = new Thread(runnable2, "T2");
+        thread1.start();
+        thread2.start();
+        sleep(1000);
+        System.out.println("Thread T1 " + thread1.getState());
+        System.out.println("Thread T2 " + thread2.getState());
+
+    }
+
+    private void operation(Object LOCK1, Object LOCK2) {
+        print("in operation() method");
         synchronized (LOCK1) {
-            print(Thread.currentThread().getName());
+            print("(lock on object 1) " + LOCK1);
             sleep(500);
-            operation2();
-        }
-    }
-
-    private void operation2() {
-        synchronized (LOCK2) {
-            print(Thread.currentThread().getName());
-            operation1();
+            synchronized (LOCK2) {
+                print("(lock on object 2) " + LOCK2);
+            }
         }
     }
 
     // helper methods
+
     private void sleep(int i) {
         try {
             Thread.sleep(i);
@@ -35,6 +52,6 @@ public class DeadlockExample1 {
     }
 
     private void print(String s) {
-        System.out.println(s);
+        System.out.println("Thread " + Thread.currentThread().getName() + " " + s);
     }
 }
