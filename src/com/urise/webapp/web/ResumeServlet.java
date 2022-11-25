516b8189a8e7e24c6cf33508cb6e3c62748742e1
Maksim Shibanov <asusib74@gmail.com>
2022-11-25 13:46:46 +0500

    HW 16.0.3 Implements jsp

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,8 +1,6 @@
 package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
-import com.urise.webapp.model.ContactType;
-import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -11,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -29,46 +25,7 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-
-        PrintWriter writer = response.getWriter();
-        writer.write("""
-                <html>
-                       <head>
-                           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-                           <title>Список всех резюме</title>
-                       </head>
-                       <body>
-                       <section>
-                       <table border="1" cellpadding = "8" cellspacing="0">
-                          <caption><h3>Список всех резюме</h3></caption>
-                          <tr>
-                           <th>Имя</th>
-                           <th>Email</th>
-                          </tr>
-                """
-        );
-
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr>\n" +
-                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
-                            "     <td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
-                            "</tr>\n");
-        }
-
-        writer.write("""
-                        </table>
-                        </section>
-                        </body >
-                    </html >
-                """
-        );
-
-//        String name = request.getParameter("name");
-//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 }
